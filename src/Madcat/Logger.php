198e889f95115c9a0cea1cba72f1ca45dcd29a57
meodien99 <seb.madcat@gmail.com>
2014-10-18 00:29:28 +0700

    add write method

--- src/Madcat/Logger.php
+++ src/Madcat/Logger.php
@@ -2,7 +2,23 @@
 
 namespace Madcat;
 
-
+use Madcat\Handler\HandlerInterface;
 class Logger {
+    protected $handlers = array();
 
+    public function registerHandler($name,HandlerInterface $handler) {
+        $this->handlers[$name] = $handler;
+    }
+
+    public function getHandlers($name) {
+        return $this->handlers[$name];
+    }
+
+    public function log($message) {
+
+        $date = date('r');
+        foreach ($this->handlers as $handler) {
+            $handler->write($date, $message);
+        }
+    }
 } 
--- tests/Madcat/LoggerTest.php
+++ tests/Madcat/LoggerTest.php
@@ -13,4 +13,43 @@ class LoggerTest extends PHPUnit_Framework_TestCase{
     public function testInstance(){
         $this->assertInstanceOf('\Madcat\Logger', $this->logger);
     }
-} 
+
+    public function testRegisterHandlerCorrectly(){
+        $handler = $this->getHandlerMock();
+        $this->logger->registerHandler('memory', $handler);
+        $this->assertSame($handler, $this->logger->getHandlers('memory'));
+    }
+
+    public function testRegisterMoreThanOneHandlerCorrectly(){
+        $handler = $this->getHandlerMock();
+        $handler2 = $this->getHandlerMock();
+        $handler3 = $this->getHandlerMock();
+
+        $this->logger->registerHandler('memory', $handler);
+        $this->logger->registerHandler('memory2', $handler2);
+        $this->logger->registerHandler('memory3', $handler3);
+
+        $this->assertSame($handler, $this->logger->getHandlers('memory'));
+        $this->assertSame($handler2, $this->logger->getHandlers('memory2'));
+        $this->assertSame($handler3, $this->logger->getHandlers('memory3'));
+    }
+
+    public function testPassingCallsToHandlers() {
+        $mock = $this->getHandlerMock();
+
+        $mock->expects($this->exactly(2))
+                ->method('write')
+                ->with (
+                    $this->isType('string'),
+                    $this->equalTo('Hello!')
+                );
+        $this->logger->registerHandler('memory', $mock);
+        $this->logger->registerHandler('memory2', $mock);
+
+        $this->logger->log('Hello!');
+    }
+
+    protected function getHandlerMock(){
+        return  $mock = $this->getMock('\Madcat\Handler\HandlerInterface');
+    }
+}
